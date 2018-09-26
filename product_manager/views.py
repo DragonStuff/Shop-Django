3167c007513e16521811a872a299ef30d9295437
DragonStuff <me@0xdragon.com>
2018-09-27 06:31:19 +1000

    Added docstrings, removed unneeded views and added DeleteView to imports.

--- product_manager/views.py
+++ product_manager/views.py
@@ -1,45 +1,47 @@
-from django.views.generic import DetailView, ListView, UpdateView, CreateView
+""" This file controls the views for Products and Categories inside product_manager. """
+
+# Django imports
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, UpdateView, CreateView, DeleteView
+
+# Project imports
 from .models import Product, Category
-from .forms import ProductForm, CategoryForm
+from .forms import ProductForm
 
 
 class ProductListView(ListView):
+    """ Allows you to list all products. """
     model = Product
 
 
 class ProductCreateView(CreateView):
+    """ Allows you to create a product (linked to forms). """
     model = Product
     form_class = ProductForm
 
 
 class ProductDetailView(DetailView):
+    """ Allows you to view detailed information about an object in Product. """
     model = Product
 
 
 class ProductDeletionView(DeleteView):
+    """ Allows you to delete a product. """
     model = Product
     success_url = reverse_lazy('product_manager_product_list')
 
 
 class ProductUpdateView(UpdateView):
+    """ Allows you to update a product's details. """
     model = Product
     form_class = ProductForm
 
 
 class CategoryListView(ListView):
+    """ Allows you to view all categories. """
     model = Category
-
-
-class CategoryCreateView(CreateView):
-    model = Category
-    form_class = CategoryForm
 
 
 class CategoryDetailView(DetailView):
+    """ Allows you to view details of a category (will be used for inline product display). """
     model = Category
-
-
-class CategoryUpdateView(UpdateView):
-    model = Category
-    form_class = CategoryForm
-
